a0eb4f55f8baf57185583a8d5857c2e5c7903e49
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-08-09 20:19:50 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,7 +43,8 @@ def turn(board)
   user_input = gets.strip
 index = input_to_index(user_input)   
   
-
+if valid_move?(board,index) == true 
+  move (board, index, character = "X" )
   
   if valid_move?(board,index) == false 
     puts "Please enter 1-9:"
